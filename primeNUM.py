b4fa5c3e1cfd488d0e3964a3604e8f22a3bf8aca
manish831 <manish.agrahari2015@gmail.com>
2020-11-27 08:43:59 +0530

    Add files via upload

---
+++ primeNUM.py
@@ -0,0 +1,14 @@
+a = int(input("Enter your number.: "))
+if a==1 or a==2 or a==3:
+    print("PRIME NUMBER")
+
+for num in range(2,a):
+        if a % num ==0 :
+            print(f"Entered {a}  is not prime number.")
+            break
+        elif a%(num-1):
+            print("Entered number is prime.")
+            break
+        else:
+            continue
+
